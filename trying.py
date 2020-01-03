b06ceb751ad23a074f148359ca5701231c7490a4
Caio Costa <49376271+caiodasilvacosta@users.noreply.github.com>
2020-01-02 19:30:12 -0500

    Update trying.py

--- trying.py
+++ trying.py
@@ -2,6 +2,6 @@ print('Hello World!')
 idade = input('Qual é a sua idade?')
 futuro = int(idade) + 10
 futurotexto = str(futuro)
-print('Daqui a 10 anos, você terá')
-print(futurotexto)
+print("Daqui a 10 anos, você terá" + futurotexto)
+
 
